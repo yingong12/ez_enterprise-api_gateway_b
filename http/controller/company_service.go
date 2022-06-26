76a896786e0b6f27734f24d740f7f32cc434b5e4
CoolGuy Yin <yingeekyin@gmai.com>
2022-06-26 18:39:22 +0800

    1

--- http/controller/company_service.go
+++ http/controller/company_service.go
@@ -20,6 +20,11 @@ func ForwardCompanyService(ctx *gin.Context) {
 		STDWrapperRaw(EnterpriseUpdate)(ctx)
 		return
 	}
+	//机构更新
+	if ctx.Request.URL.Path == "/group/update" || ctx.Request.URL.Path == "group/update" {
+		STDWrapperRaw(GroupUpdate)(ctx)
+		return
+	}
 	Proxy(ctx, env.GetStringVal("LB_COMPANY_SERVICE"))
 }
 
@@ -54,6 +59,55 @@ func EnterpriseUpdate(ctx *gin.Context) (res RawResponse, err error) {
 	client := providers.HttpClientCompanyService
 	//从url里拿appID
 	URL := fmt.Sprintf("%s/enterprise/%s", client.BaseURL, appID)
+	request, err := http.NewRequest("PUT", URL, clientBody)
+	if err != nil {
+		return
+	}
+	rsp, err := client.Do(request)
+	if err != nil {
+		return
+	}
+	//解析rsp
+	data, err := ioutil.ReadAll(rsp.Body)
+	if err != nil {
+		return
+	}
+	//
+	res = data
+	return
+}
+
+//更新机构
+func GroupUpdate(ctx *gin.Context) (res RawResponse, err error) {
+	clientBody := &bytes.Reader{}
+	v, _ := ctx.Get("auth_info")
+	appID := v.(*middleware.AuthInfo).AppID
+	if appID == "" {
+		ctx.JSON(200, gin.H{
+			"code": buz_code.CODE_AUTH_FAILED,
+			"msg":  "该用户还没绑定企业或机构",
+		})
+		return
+	}
+	if err = func() error {
+		body := ctx.Request.Body
+		bodyMap := map[string]interface{}{}
+		err := json.NewDecoder(body).Decode(&bodyMap)
+		if err != nil {
+			return fmt.Errorf("decode failed")
+		}
+		j, _ := json.Marshal(bodyMap)
+		clientBody = bytes.NewReader(j)
+		return nil
+	}(); err != nil {
+		//直接把错误抛给后端
+		res = []byte(err.Error())
+		return
+	}
+	//发送请求
+	client := providers.HttpClientCompanyService
+	//从url里拿appID
+	URL := fmt.Sprintf("%s/group/%s", client.BaseURL, appID)
 	request, err := http.NewRequest("PUT", URL, clientBody)
 	if err != nil {
 		return
